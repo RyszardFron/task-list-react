d947bc90802e8da2884e940102c75af56d4c2501
Ryszard Fron <ryszard.fron1973@gmail.com>
2023-02-17 22:12:56 +0100

    adding  Form component

---
+++ src/Form/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const Form = () => (
+    <form className="addNewTask__form">
+            <input className="addNewTask__create" placeholder="Co jest do zrobienia?" autofocus />
+            <button className="addNewTask__button">Dodaj zadanie</button>
+          </form>
+)
+
+export default Form;
