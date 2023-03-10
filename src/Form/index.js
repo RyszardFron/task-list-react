6aee1f4eff511a7d77a034ae09690f7848a4da0d
Ryszard Fron <ryszard.fron1973@gmail.com>
2023-03-10 22:14:35 +0100

    src->Form class change according to BEM . Format

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,14 @@
 import "./style.css";
 
 const Form = () => (
-    <form className="addNewTask__form">
-            <input className="addNewTask__create" placeholder="Co jest do zrobienia?" autofocus />
-            <button className="addNewTask__button">Dodaj zadanie</button>
-          </form>
+  <form className="addNewTask">
+    <input className="addNewTask__create"
+      placeholder="Co jest do zrobienia?" 
+      autofocus />
+    <button className="addNewTask__button">
+      Dodaj zadanie
+      </button>
+  </form>
 )
 
 export default Form;
