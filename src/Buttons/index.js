50e220acb58426b8efcba01ca58de9f5a60363b0
Ryszard Fron <ryszard.fron1973@gmail.com>
2023-03-10 20:55:28 +0100

    src->Buttons; changing the "buttons" class according to BEM, formatting

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,23 +1,21 @@
-import React from "react";
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
-
     <div className="section__buttons">
         {tasks.length > 0 && (
             <>
-                <button className="buttons">
-                    {hideDoneTasks ? "Pokaż ukończone" : "Ukryj ukończone"}
+                <button className="buttons__button">
+                    {hideDoneTasks 
+                    ? "Pokaż ukończone" 
+                    : "Ukryj ukończone"}
                 </button>
-                <button className="buttons"
-                    disabled={tasks.every(({ done }) => done)}
-                >
+                <button className="buttons__button"
+                    disabled={tasks.every(({ done }) => done)}>
                     Ukończ wszystkie
                 </button>
             </>
         )}
     </div >
 );
-
 
         export default Buttons;
